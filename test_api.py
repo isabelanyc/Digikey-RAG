65f5d0d14f388cbe2c5c6523c630947b44b816a3
Isabela Caetano <isabelanyc@gmail.com>
2023-08-17 13:44:56 -0400

    created test api file

--- test_api.py
+++ test_api.py
@@ -1,21 +1,23 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import digikey
+from digikey.v3.productinformation import KeywordSearchRequest
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Use the loaded environment variables
+# Get environment variables
+CLIENT_ID = os.getenv('DIGIKEY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('DIGIKEY_CLIENT_SECRET')
+CLIENT_SANDBOX = os.getenv('DIGIKEY_CLIENT_SANDBOX')
 CACHE_DIR = Path(os.getenv('DIGIKEY_STORAGE_PATH'))
 
-# Set the other environment variables (no need to use os.environ)
-DIGIKEY_CLIENT_ID = os.getenv('DIGIKEY_CLIENT_ID')
-DIGIKEY_CLIENT_SECRET = os.getenv('DIGIKEY_CLIENT_SECRET')
-DIGIKEY_CLIENT_SANDBOX = os.getenv('DIGIKEY_CLIENT_SANDBOX')
-
-import digikey
-from digikey.v3.productinformation import KeywordSearchRequest
-from digikey.v3.batchproductdetails import BatchProductDetailsRequest
+# Set environment variables
+os.environ['DIGIKEY_CLIENT_ID'] = CLIENT_ID
+os.environ['DIGIKEY_CLIENT_SECRET'] = CLIENT_SECRET
+os.environ['DIGIKEY_CLIENT_SANDBOX'] = CLIENT_SANDBOX
+os.environ['DIGIKEY_STORAGE_PATH'] = str(CACHE_DIR)
 
 # Query product number
 dkpn = '296-6501-1-ND'
@@ -24,9 +26,3 @@ part = digikey.product_details(dkpn)
 # Search for parts
 search_request = KeywordSearchRequest(keywords='CRCW080510K0FKEA', record_count=10)
 result = digikey.keyword_search(body=search_request)
-
-# Only if BatchProductDetails endpoint is explicitly enabled
-# Search for Batch of Parts/Product
-mpn_list = ["0ZCK0050FF2E", "LR1F1K0"] #Length upto 50
-batch_request = BatchProductDetailsRequest(products=mpn_list)
-part_results = digikey.batch_product_details(body=batch_request)
